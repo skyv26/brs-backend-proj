e708505970838ba9cdec003332d02bbf9c04bf15
Mohamed Abdulhakim <abdulhakimm280@gmail.com>
2023-06-07 14:23:37 +0200

    Create associations for route and station

--- app/models/station.rb
+++ app/models/station.rb
@@ -1,6 +1,7 @@
 class Station < ApplicationRecord
   include StringValidator
-
+  has_many :routes, foreign_key: 'start_station_id'
+  has_many :routes, foreign_key: 'destination_station_id'
   validates :name, presence: true, string: true
   validates :city, presence: true, string: true
   validates :state, presence: true, string: true
