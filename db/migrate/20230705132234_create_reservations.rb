6b475410dbf9765758f7830ec4fe7377067dcfec
Aakash Verma <dev.aakashv@gmail.com>
2023-07-05 18:52:55 +0530

    Generate the `Reservation` migration

---
+++ db/migrate/20230705132234_create_reservations.rb
@@ -0,0 +1,12 @@
+class CreateReservations < ActiveRecord::Migration[7.0]
+  def change
+    create_table :reservations do |t|
+      t.integer :berth_number
+      t.string :refund_status
+      t.decimal :amount_paid
+      t.boolean :status
+
+      t.timestamps
+    end
+  end
+end
