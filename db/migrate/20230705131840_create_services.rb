2a8f8511f0efc5b592350b6d277a2a71e0044173
Aakash Verma <dev.aakashv@gmail.com>
2023-07-05 18:50:22 +0530

    Generate the `Service` migration

---
+++ db/migrate/20230705131840_create_services.rb
@@ -0,0 +1,10 @@
+class CreateServices < ActiveRecord::Migration[7.0]
+  def change
+    create_table :services, id: :uuid do |t|
+      t.string :name
+      t.string :icon
+
+      t.timestamps
+    end
+  end
+end
