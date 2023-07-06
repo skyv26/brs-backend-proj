f7385e4b92857fc8b81c23613fa2ca55fa361673
Aakash Verma <dev.aakashv@gmail.com>
2023-07-06 18:32:04 +0530

    add the `validation` for the `reservation` amount_paid column

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,5 @@
 class Reservation < ApplicationRecord
     validates :berth_number, presence: true, numericality: true
     validates :refund_status, presence: true
+    validates :amount_paid, presence: true, numericality: true
 end
