0c95c89339c7d83b47bd1a1e15188f3a5dbff02b
Aakash Verma <dev.aakashv@gmail.com>
2023-07-10 18:57:41 +0530

    Add the `model` validation for status column

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,6 +1,11 @@
 class Reservation < ApplicationRecord
+    
     validates :berth_number, presence: true, numericality: true
     validates :refund_status, presence: true
     validates :amount_paid, presence: true, numericality: true
-    validates :status, presence: true, boolean: true
+    validate :status_check_func
+      
+    def status_check_func
+        puts status.to_s
+    end
 end
