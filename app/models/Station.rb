26d02fbe8d35e76dc82c334480b3bf697ba73751
Aakash Verma <dev.aakashv@gmail.com>
2023-07-11 19:08:57 +0530

    Follow the naming convention and rename the model name using snake_case

--- app/models/Station.rb
+++
@@ -1,7 +0,0 @@
-class Station < ApplicationRecord
-  include StringValidator
-
-  validates :name, presence: true, string: true
-  validates :city, presence: true, string: true
-  validates :state, presence: true, string: true
-end
