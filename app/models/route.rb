6bf293156b302b89491634dd950eb1f78240fac8
strangeal <bajerijustice@gmail.com>
2023-07-11 12:42:54 +0000

    Add model validation for route

--- app/models/route.rb
+++ app/models/route.rb
@@ -1,2 +1,14 @@
 class Route < ApplicationRecord
+  validates :distance, presence: true, numericality: { only_integer: true }
+  validates :time_duration, presence: true, numericality: { only_integer: true }
+  validates :departure_time, presence: true
+  validate :departure_time_is_valid_datetime
+
+  def departure_time_is_valid_datetime
+    begin
+      DateTime.parse(departure_time.to_s)
+    rescue ArgumentError
+      errors.add(:departure_time, "must be a valid datetime")
+    end
+  end
 end
