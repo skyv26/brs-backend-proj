aa93561574c31e0293866718d49304d02c1c4b91
Aakash Verma <dev.aakashv@gmail.com>
2023-07-13 14:15:23 +0530

    feat: update the validation of `refund_status`

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,7 +1,7 @@
 class Reservation < ApplicationRecord
   include BooleanValidator
   validates :berth_number, presence: true, numericality: true
-  validates :refund_status, presence: true
+  validates :refund_status, presence: true, inclusion: { in: %w(string values) }
   validates :amount_paid, presence: true, numericality: true
   validates :status, presence: true, boolean: true
 end
