ab532ed4029389d15a236b8a51d09eaecac02b87
Mohamed Abdulhakim <abdulhakimm280@gmail.com>
2023-07-24 16:54:48 +0200

    update tthe bus spec

--- spec/models/bus_spec.rb
+++ spec/models/bus_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Bus, type: :model do
-<<<<<<< HEAD
   before(:each) do
     @bus = Bus.create(name: 'Bus 1', bus_number: '1001', capacity: 50, enquiry_number: '12fd')
   end
@@ -25,30 +24,5 @@ RSpec.describe Bus, type: :model do
       @bus.name = 'Bus hjvfdsdszdsxgf1hgfdchgfhtcghhchfvgchrsrshdjhgyj'
       expect(@bus).to_not be_valid
     end
-=======
-  subject { described_class.new(name: 'Bus1', enquiry_number: '1234567890', bus_number: 'TN01AB1234', capacity: 50) }
-  it 'is valid with valid attributes' do
-    expect(subject).to be_valid
-  end
-
-  it 'is not valid without a name' do
-    subject.name = nil
-    expect(subject).to_not be_valid
-  end
-
-  it 'is not valid without a bus_number' do
-    subject.bus_number = nil
-    expect(subject).to_not be_valid
-  end
-
-  it 'is not valid without a capacity' do
-    subject.capacity = nil
-    expect(subject).to_not be_valid
-  end
-
-  it 'is not valid with a name longer than 50 characters' do
-    subject.name = 'a' * 51
-    expect(subject).to_not be_valid
->>>>>>> 4f34396 (Resolve Linters)
   end
 end
