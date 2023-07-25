b9ca455ef693364d3e1ef0099709ba67fdeb9a9b
Aakash Verma <dev.aakashv@gmail.com>
2023-07-25 19:10:14 +0530

    Resolve the `rubocop` errors

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,5 +1,4 @@
 class Reservation < ApplicationRecord
-  
   validates :berth_number, presence: true, numericality: true
   validates :refund_status, presence: true
   validates :amount_paid, presence: true, numericality: { greater_than_or_equal_to: 0 }
