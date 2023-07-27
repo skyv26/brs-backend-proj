3e026123fb70c2126431889f63e96a2fd61b7e51
Aakash Verma <dev.aakashv@gmail.com>
2023-07-27 18:53:36 +0530

    Update the `Service` controller by adding the actions definition

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,19 +1,43 @@
 class ServicesController < ApplicationController
+    attr_reader :service_params, :fetch_service
+    before_action :fetch_service, only: [:update, :show, :destroy]
+
     def index
+        Service.all
     end
     
     def show
+        render json: fetch_service
     end
 
     def new
+        Service.new
     end
 
     def create
+        @service = Service.new(service_params)
+        if @service.save
+            render json: @service
+        end
     end
 
-    def delete
+    def destroy
+        @service = fetch_service
+        if @service.delete
+            render json: @service
+        end
     end
 
     def update
+    end
+
+    private
+
+    def fetch_service
+        Service.find(params[:id])
+    end
+
+    def service_params
+        params.require(:service).permit('name', 'icon')
     end
 end
