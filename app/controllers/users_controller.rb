7ddd8d21d5b858d4434cbb9393debc275d61f95f
Aakash Verma <dev.aakashv@gmail.com>
2023-07-27 18:28:49 +0530

    Add the `Reservation` controller action methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,19 @@
 class UsersController < ApplicationController
+    def index
+    end
+    
+    def show
+    end
+
+    def new
+    end
+
+    def create
+    end
+
+    def delete
+    end
+
+    def update
+    end
 end
