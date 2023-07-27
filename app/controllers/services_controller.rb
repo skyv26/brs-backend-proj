194e69fed5d302a910d5305110996770a56309f8
Aakash Verma <dev.aakashv@gmail.com>
2023-07-27 18:28:26 +0530

    Add the `Service` controller action methods

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,2 +1,19 @@
 class ServicesController < ApplicationController
+    def index
+    end
+    
+    def show
+    end
+
+    def new
+    end
+
+    def create
+    end
+
+    def delete
+    end
+
+    def update
+    end
 end
