16d6f917d5a2956a59d146d4e98a9612e943cbc2
Aakash Verma <dev.aakashv@gmail.com>
2023-08-03 17:49:01 +0530

    Add the `controller` endpoints logic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,9 @@ class UsersController < ApplicationController
     end
     
     def show
+        obj = JSON.parse(UserSerializer.new(fetch_user).serialized_json)
+        obj[:status] = :ok
+        render json: obj
     end
 
     def create
@@ -27,10 +30,35 @@ class UsersController < ApplicationController
         end
     end
 
-    def delete
+    def destroy
+        @user = fetch_user
+        obj = {}
+        if @user.delete
+            obj = JSON.parse(UserSerializer.new(@user).serialized_json)
+            obj[:status] = :ok
+            obj[:message] = "User is deleted successfully !"
+            render json: obj
+        else
+            obj[:status] = :forbidden
+            obj[:message] = "Please make sure that your ID is correct"
+            render json: obj
+        end
     end
 
     def update
+        @user = fetch_user
+        @user.update(user_params)
+        obj = {}
+        if @user.save
+            obj = JSON.parse(UserSerializer.new(@user).serialized_json)
+            obj[:status] = :ok
+            obj[:message] = "User is updated successfully"
+            render json: obj
+        else
+            obj[:status] = :unprocessable_entity
+            obj[:message] = "Please make sure that your ID is correct"
+            render json: obj
+        end
     end
 
 
