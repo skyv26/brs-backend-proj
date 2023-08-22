37c738599e413b6501f8ce33127a3ca19c2a553f
muskan2532150 <rg2532150@gmail.com>
2023-08-22 18:16:51 +0530

    Generate route_spec file for swagger

--- spec/requests/routes_spec.rb
+++ spec/requests/routes_spec.rb
@@ -1,7 +1,118 @@
-require 'rails_helper'
+require 'swagger_helper'
 
-RSpec.describe 'Routes', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe 'api/v1/routes', type: :request do
+  path '/api/v1/routes' do
+    get('List Routes') do
+      tags 'Routes'
+      response(200, 'successful') do
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+
+    post('Create New Route') do
+      tags 'Routes'
+      consumes 'application/json'
+      parameter name: :route, in: :body, schema: {
+        type: :object,
+        properties: {
+          route: {
+            type: :object,
+            properties: {
+              distance: { type: :integer },
+              time_duration: { type: :integer },
+              departure_time: { type: :string },
+            },
+            required: %w[distance time_duration departure_time],
+          },
+        },
+      }
+
+      response(200, 'successful') do
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+
+  path '/api/v1/routes/{id}' do
+    # You'll want to customize the parameter types...
+    parameter name: 'id', in: :path, type: :string, description: 'id'
+
+    get('Show Route') do
+      tags 'Routes'
+      response(200, 'successful') do
+        let(:id) { '123' }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+
+    put('Update Route by ID') do
+      tags 'Routes'
+      parameter name: 'id', in: :path, type: :string, description: "Update a record by Id"
+      consumes 'application/json'
+      parameter name: :route, in: :body, schema: {
+        type: :object,
+        properties: {
+          route: {
+            type: :object,
+            properties: {
+              distance: { type: :integer },
+              time_duration: { type: :integer },
+              departure_time: { type: :string },
+            },
+          },
+        },
+      }
+
+      response(200, 'successful') do
+        let(:id) { '123' }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+
+    delete('Delete Route') do
+      tags 'Routes'
+      response(200, 'successful') do
+        let(:id) { '123' }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
   end
 end
