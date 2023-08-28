f3187327931534f76f4436fb214acfe2fe4aa3de
Aakash Verma <dev.aakashv@gmail.com>
2023-08-29 01:10:48 +0530

    Remove the `old` test cases

--- spec/models/route_spec.rb
+++ spec/models/route_spec.rb
@@ -63,10 +63,6 @@ RSpec.describe Route, type: :model do
         expect(route).to_not be_valid
       end
 
-      # it 'departure_time should be in Date and Time format' do
-      #   route[:time_duration] = 1234
-      #   expect(route).to_not be_valid
-      # end
     end
   end
 
@@ -77,24 +73,4 @@ RSpec.describe Route, type: :model do
       end
     end
   end
-  # subject(:route) { described_class.new(distance: 100, time_duration: 120, departure_time: '2023-07-25 10:00:00') }
-
-  # describe 'validation' do
-  #   it { is_expected.to validate_presence_of(:distance) }
-  #   it { is_expected.to validate_numericality_of(:distance).only_integer }
-  #   it { is_expected.to validate_presence_of(:time_duration) }
-  #   it { is_expected.to validate_numericality_of(:time_duration).only_integer }
-  #   it { is_expected.to validate_presence_of(:departure_time) }
-
-  #   context 'When departure_time is not a valid datetime' do
-  #     subject(:invalid_route) do
-  #       described_class.new(distance: 100, time_duration: 120, departure_time: 'not a datetime')
-  #     end
-
-  #     it 'is not valid' do
-  #       expect(invalid_route).not_to be_valid
-  #       expect(invalid_route.errors[:departure_time]).to include('must be a valid datetime')
-  #     end
-  #   end
-  # end
 end
