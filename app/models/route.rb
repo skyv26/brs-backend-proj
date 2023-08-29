95859205a298c5311a666983a04249c4880bd4cd
Aakash Verma <dev.aakashv@gmail.com>
2023-08-30 00:31:45 +0530

    Improve the `validation` for the columns

--- app/models/route.rb
+++ app/models/route.rb
@@ -1,30 +1,40 @@
 class Route < ApplicationRecord
   belongs_to :start_station, class_name: 'Station', foreign_key: 'start_station_id'
   belongs_to :destination_station, class_name: 'Station', foreign_key: 'destination_station_id'
-  validate :distance_validity
-  validate :time_duration_validity
+  validate :validate_distance_type
+  validate :validate_time_duration_type
   validates :departure_time, presence: true
-  validate :departure_time_is_valid_datetime
+  validate :departure_time_validity
 
   has_many :bus_routes
   has_many :buses, through: :bus_routes
 
   private
-
-  def distance_validity
-    errors.add(:distance, 'is a required property') unless distance.present?
-
-    return unless distance_before_type_cast.is_a?(String)
-
-    errors.add(:distance, 'must be an integer, not a string')
+  
+  def validate_distance_type
+    if distance_before_type_cast.is_a?(String) ||
+       distance_before_type_cast.is_a?(TrueClass) ||
+       distance_before_type_cast.is_a?(FalseClass) ||
+       distance_before_type_cast.is_a?(NilClass)
+       errors.add(:distance, 'must be an integer')
+    end
   end
 
-  def time_duration_validity
+  def validate_distance_presence
+    errors.add(:distance, 'is a required property') unless distance.present?
+  end
+
+  def validate_time_duration_type
+    if time_duration_before_type_cast.is_a?(String) ||
+       time_duration_before_type_cast.is_a?(TrueClass) ||
+       time_duration_before_type_cast.is_a?(FalseClass) ||
+       time_duration_before_type_cast.is_a?(NilClass)
+      errors.add(:time_duration, 'must be an integer')
+    end
+  end
+
+  def validate_time_duration_presence
     errors.add(:time_duration, 'is a required property') unless time_duration.present?
-
-    return unless time_duration_before_type_cast.is_a?(String)
-
-    errors.add(:time_duration, 'must be an integer, not a string')
   end
 
   def departure_time_validity
