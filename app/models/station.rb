0e4bcf80bf8d822f5973ae0f945dce0aad4804cb
Aakash Verma <dev.aakashv@gmail.com>
2023-08-29 21:35:33 +0530

    Add the `custom` validations for the `Station`

--- app/models/station.rb
+++ app/models/station.rb
@@ -2,7 +2,43 @@ class Station < ApplicationRecord
   include StringValidator
   has_many :routes, foreign_key: 'start_station_id'
   has_many :routes, foreign_key: 'destination_station_id'
-  validates :name, presence: true, string: true
-  validates :city, presence: true, string: true
-  validates :state, presence: true, string: true
+  validate :name_should_be_string
+  validate :city_should_be_string
+  validate :state_should_be_string
+
+  def name_should_be_string
+    unless name.present?
+      errors.add(:name, "is a required property")
+    end
+
+    if name_before_type_cast.is_a?(Integer) || name_before_type_cast.is_a?(TrueClass) || name_before_type_cast.is_a?(FalseClass) || name_before_type_cast.is_a?(NilClass)
+      errors.add(:name, "must be a string")
+    elsif name.size > 20
+      errors.add(:name, "should not be greater than 20 characters")
+    end
+  end
+
+  def city_should_be_string
+    unless city.present?
+      errors.add(:city, "is a required property")
+    end
+    
+    if city_before_type_cast.is_a?(Integer) || city_before_type_cast.is_a?(TrueClass) || city_before_type_cast.is_a?(FalseClass) || city_before_type_cast.is_a?(NilClass)
+      errors.add(:city, "must be a string")
+    elsif city.size > 20
+      errors.add(:city, "should not be greater than 20 characters")
+    end
+  end
+
+  def state_should_be_string
+    unless state.present?
+      errors.add(:state, "is a required property")
+    end
+
+    if state_before_type_cast.is_a?(Integer) || state_before_type_cast.is_a?(TrueClass) || state_before_type_cast.is_a?(FalseClass) || state_before_type_cast.is_a?(NilClass)
+      errors.add(:state, "must be a string")
+    elsif state.length > 20
+      errors.add(:state, "should not be greater than 20 characters")
+    end
+  end
 end
