0fd706b9d94e66ba34e6ef780dd5bfc502a8e531
Aakash Verma <dev.aakashv@gmail.com>
2023-08-30 00:36:01 +0530

    Remove the `tags`

--- spec/requests/api/v1/routes_spec.rb
+++ spec/requests/api/v1/routes_spec.rb
@@ -53,11 +53,9 @@ RSpec.describe 'api/v1/routes', type: :request do
   end
 
   path '/api/v1/routes/{id}' do
-    # You'll want to customize the parameter types...
     parameter name: 'id', in: :path, type: :string, description: 'id'
 
     get('Show Route') do
-      tags 'Routes'
       response(200, 'successful') do
         after { |example| add_response(example) }
         run_test!
@@ -65,7 +63,6 @@ RSpec.describe 'api/v1/routes', type: :request do
     end
 
     put('Update Route by ID') do
-      tags 'Routes'
       consumes 'application/json'
       parameter route_parameter_schema
       response(200, 'successful') do
@@ -82,7 +79,6 @@ RSpec.describe 'api/v1/routes', type: :request do
     end
 
     delete('Delete Route') do
-      tags 'Routes'
       response(200, 'successful') do
         let(:id) { '123' }
         after { |example| add_response(example) }
