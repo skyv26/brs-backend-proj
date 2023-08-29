205808eeb5e2a83d4fbeabd53c4fa0e9e0a993d4
Aakash Verma <dev.aakashv@gmail.com>
2023-08-30 00:38:54 +0530

    Use custom functions

--- spec/requests/api/v1/stations_spec.rb
+++ spec/requests/api/v1/stations_spec.rb
@@ -1,7 +1,7 @@
 require 'swagger_helper'
 require 'response_helper'
 
-def station_parameter_schema
+def station_parameter_schema(required_properties = [])
   {
     name: :station,
     in: :body,
@@ -14,7 +14,8 @@ def station_parameter_schema
             name: { type: :string },
             city: { type: :string },
             state: { type: :string }
-          }
+          },
+          required: required_properties
         }
       }
     }
@@ -36,20 +37,7 @@ RSpec.describe 'api/v1/stations', type: :request do
     post('create station') do
       tags 'Stations'
       consumes 'application/json'
-      parameter name: :station, in: :body, schema: {
-        type: :object,
-        properties: {
-          station: {
-            type: :object,
-            properties: {
-              name: { type: :string },
-              city: { type: :string },
-              state: { type: :string }
-            },
-            required: %w[name city state]
-          }
-        }
-      }
+      parameter station_parameter_schema(%w[name city state])
       response(200, 'successful') do
         after { |example| add_response(example) }
         run_test!
