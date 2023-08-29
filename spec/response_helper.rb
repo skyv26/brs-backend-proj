02a4ec92f05552595504184abfec4ab4d1b57d0b
Aakash Verma <dev.aakashv@gmail.com>
2023-08-29 23:45:00 +0530

    Resolve the private method issue

--- spec/response_helper.rb
+++ spec/response_helper.rb
@@ -1,11 +1,10 @@
 # response_helper.rb
 module ResponseHelper
-    def add_response(example)
-        example.metadata[:response][:content] = {
-            'application/json' => {
-                example: JSON.parse(response.body, symbolize_names: true)
-            }
-        }
-    end
+  def add_response(example)
+    example.metadata[:response][:content] = {
+      'application/json' => {
+        example: JSON.parse(response.body, symbolize_names: true)
+      }
+    }
+  end
 end
-  
