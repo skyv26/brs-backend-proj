759acf7e2ce39ff9f537c133ffc18bae664413b4
Aakash Verma <dev.aakashv@gmail.com>
2023-08-29 21:35:55 +0530

    Add the `Model` spec tests

--- spec/models/station_spec.rb
+++ spec/models/station_spec.rb
@@ -2,25 +2,91 @@ require 'rails_helper'
 
 RSpec.describe Station, type: :model do
   subject { described_class.new(name: 'Station1', city: 'FTN', state: 'TN01AB1234') }
+  context 'table test cases checking for either bad argument or invalid information.' do
+    describe Station do
+      it 'name should not be nil' do
+        subject.name = nil
+        expect(subject).to_not be_valid
+      end
 
-  describe 'Validations' do
-    it 'is valid with valid attributes' do
-      expect(subject).to be_valid
+      it 'name should not be integer' do
+        subject.name = 1234
+        expect(subject).to_not be_valid
+      end
+
+      it 'name should not be greater than 20 characters' do
+        subject.name = 'test' * 6
+        expect(subject).to_not be_valid
+      end
+
+      it 'name should not be boolean' do
+        subject.name = true
+        expect(subject).to_not be_valid
+      end
+
+      it 'name should not be boolean' do
+        subject.name = false
+        expect(subject).to_not be_valid
+      end
+
+      it 'city should not be nil' do
+        subject.city = nil
+        expect(subject).to_not be_valid
+      end
+
+      it 'city should not be integer' do
+        subject.city = 1234
+        expect(subject).to_not be_valid
+      end
+
+      it 'city should not be greater than 20 characters' do
+        subject.city = 'test' * 6
+        expect(subject).to_not be_valid
+      end
+
+      it 'city should not be boolean' do
+        subject.city = true
+        expect(subject).to_not be_valid
+      end
+
+      it 'city should not be boolean' do
+        subject.city = false
+        expect(subject).to_not be_valid
+      end
+
+      it 'state should not be nil' do
+        subject.state = nil
+        expect(subject).to_not be_valid
+      end
+
+      it 'state should not be integer' do
+        subject.state = 1234
+        expect(subject).to_not be_valid
+      end
+
+      it 'state should not be greater than 20 characters' do
+        subject.state = 'test' * 6
+        expect(subject).to_not be_valid
+      end
+
+      it 'state should not be boolean' do
+        subject.state = true
+        expect(subject).to_not be_valid
+      end
+
+      it 'state should not be boolean' do
+        subject.state = false
+        expect(subject).to_not be_valid
+      end
+
     end
+  end
 
-    it 'is not valid without a name' do
-      subject.name = nil
-      expect(subject).to_not be_valid
-    end
-
-    it 'is not valid without a city' do
-      subject.city = nil
-      expect(subject).to_not be_valid
-    end
-
-    it 'is not valid with a name longer than 50 characters' do
-      subject.name = 'a' * 51
-      expect(subject).to_not be_valid
+  context 'table test cases checking for correct' do
+    describe Station do
+      it 'should pass the test' do
+        expect(subject).to be_valid
+      end
     end
   end
 end
