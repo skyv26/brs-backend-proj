204d73a3d6a7c01df813e95348b10d262b223456
Aakash Verma <dev.aakashv@gmail.com>
2023-08-29 23:25:56 +0530

    Create the `response` helper method

---
+++ spec/response_helper.rb
@@ -0,0 +1,11 @@
+# response_helper.rb
+module ResponseHelper
+    def add_response(example)
+        example.metadata[:response][:content] = {
+            'application/json' => {
+                example: JSON.parse(response.body, symbolize_names: true)
+            }
+        }
+    end
+end
+  
