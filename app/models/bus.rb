fdd2ce24bcda22f99b560fc2a3a86c4b7b526e50
Aakash Verma <dev.aakashv@gmail.com>
2023-09-05 22:11:04 +0530

    Add the `alias` name for the `user` and added the association

--- app/models/bus.rb
+++ app/models/bus.rb
@@ -3,6 +3,7 @@ class Bus < ApplicationRecord
   validates :bus_number, presence: true, format: { with: /\A\w+\z/, message: 'should be alphanumeric' }
   validates :capacity, presence: true, numericality: { only_numeric: true } \
 
+  belongs_to :agency, class_name: 'User'  
   has_many :bus_routes
   has_many :routes, through: :bus_routes
   has_many :services, through: :busservices
