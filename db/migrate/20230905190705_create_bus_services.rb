8cc41c441bd7df7a00a4c1f284a2d69d106a33ce
Aakash Verma <dev.aakashv@gmail.com>
2023-09-06 00:51:21 +0530

    Add the `type: uuid` for the tables

--- db/migrate/20230905190705_create_bus_services.rb
+++ db/migrate/20230905190705_create_bus_services.rb
@@ -1,8 +1,8 @@
 class CreateBusServices < ActiveRecord::Migration[7.0]
   def change
     create_table :bus_services do |t|
-      t.references :bus, null: false, foreign_key: true
-      t.references :service, null: false, foreign_key: true
+      t.references :bus, null: false, foreign_key: true, type: :uuid
+      t.references :service, null: false, foreign_key: true, type: :uuid
 
       t.timestamps
     end
