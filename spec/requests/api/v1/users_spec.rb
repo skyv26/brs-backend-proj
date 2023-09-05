eab8b4e47f3c59705d1520948b315b822761d159
Aakash Verma <dev.aakashv@gmail.com>
2023-09-05 17:40:33 +0530

    Resolved the `Rubocop` errors

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -2,35 +2,33 @@ require 'swagger_helper'
 require 'response_helper'
 
 def user_parameter_schema(required_properties = [])
-    {
-      name: :user,
-      in: :body,
-      schema: {
-        type: :object,
-        properties: {
-          station: {
-            type: :object,
-            properties: {
-              full_name: { type: :string },
-              email_address: { type: :string, format: :email},
-              date_of_birth: { type: :string, format: :date},
-              mobile_no: { type: :string },
-              password: { type: :string },
-              profile_photo: { type: :string },
-              security_question: { type: :string },
-              security_answer: { type: :string }
-            },
-            required: required_properties
-          }
+  {
+    name: :user,
+    in: :body,
+    schema: {
+      type: :object,
+      properties: {
+        station: {
+          type: :object,
+          properties: {
+            full_name: { type: :string },
+            email_address: { type: :string, format: :email },
+            date_of_birth: { type: :string, format: :date },
+            mobile_no: { type: :string },
+            password: { type: :string },
+            profile_photo: { type: :string },
+            security_question: { type: :string },
+            security_answer: { type: :string }
+          },
+          required: required_properties
         }
       }
     }
-  end
+  }
+end
 
 RSpec.describe 'api/v1/users', type: :request do
-
   path '/api/v1/users' do
-
     get('list users') do
       tags 'User'
       response(200, 'successful') do
@@ -40,13 +38,13 @@ RSpec.describe 'api/v1/users', type: :request do
     end
 
     post('create user') do
-        tags 'User'
-        consumes 'application/json'
-        parameter user_parameter_schema(%w[full_name email_address])
-        response(200, 'successful') do
-            after { |example| add_response(example) }
-            run_test!
-        end
+      tags 'User'
+      consumes 'application/json'
+      parameter user_parameter_schema(%w[full_name email_address])
+      response(200, 'successful') do
+        after { |example| add_response(example) }
+        run_test!
+      end
     end
   end
 
@@ -54,31 +52,31 @@ RSpec.describe 'api/v1/users', type: :request do
     # You'll want to customize the parameter types...
     parameter name: 'id', in: :path, type: :string, description: 'id'
     get('show user') do
-        tags 'User'
-        response(200, 'successful') do
-          let(:id) { '123' }
-          after { |example| add_response(example) }
-          run_test!
-        end
+      tags 'User'
+      response(200, 'successful') do
+        let(:id) { '123' }
+        after { |example| add_response(example) }
+        run_test!
+      end
     end
     put('update user') do
-        tags 'User'
-        consumes 'application/json'
-        parameter user_parameter_schema
-        response(200, 'successful') do
-          let(:id) { '123' }
-          after { |example| add_response(example) }
-          run_test!
-        end
+      tags 'User'
+      consumes 'application/json'
+      parameter user_parameter_schema
+      response(200, 'successful') do
+        let(:id) { '123' }
+        after { |example| add_response(example) }
+        run_test!
       end
+    end
 
-      delete('delete user') do
-        tags 'User'
-        response(200, 'successful') do
-          let(:id) { '123' }
-          after { |example| add_response(example) }
-          run_test!
-        end
+    delete('delete user') do
+      tags 'User'
+      response(200, 'successful') do
+        let(:id) { '123' }
+        after { |example| add_response(example) }
+        run_test!
       end
+    end
   end
 end
