16963604effefc2bce11e8cb78fab1eca53ed512
Aakash Verma <dev.aakashv@gmail.com>
2023-09-05 18:20:22 +0530

    Fix the before_type_cast proble,

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User < ApplicationRecord
   def validate_full_name
     errors.add(:full_name, 'must be a string') unless full_name_before_type_cast.is_a?(String)
 
-    return unless full_name.is_a?(String)
+    return unless full_name_before_type_cast.is_a?(String)
 
     if full_name.size > 20
       errors.add(:full_name, 'should not be greater than 50 characters')
@@ -25,7 +25,7 @@ class User < ApplicationRecord
   def validate_email_address
     errors.add(:email_address, 'must be a string') unless email_address_before_type_cast.is_a?(String)
 
-    return unless email_address.is_a?(String)
+    return unless email_address_before_type_cast.is_a?(String)
 
     if email_address.size > 128
       errors.add(:email_address, 'should not be greater than 128 characters')
