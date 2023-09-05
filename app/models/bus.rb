af8efb51b4ec55286a3510af3591b604fc61d882
Aakash Verma <dev.aakashv@gmail.com>
2023-09-05 22:20:23 +0530

    Add the name validation for the bsu

--- app/models/bus.rb
+++ app/models/bus.rb
@@ -1,10 +1,26 @@
 class Bus < ApplicationRecord
-  validates :name, presence: true, length: { maximum: 50 }
+  validates :name, presence: true
   validates :bus_number, presence: true, format: { with: /\A\w+\z/, message: 'should be alphanumeric' }
-  validates :capacity, presence: true, numericality: { only_numeric: true } \
+  validates :capacity, presence: true, numericality: { only_numeric: true }
+
+  validate :validate_bus_name
 
   belongs_to :agency, class_name: 'User'  
   has_many :bus_routes
   has_many :routes, through: :bus_routes
   has_many :services, through: :busservices
+
+  private
+
+  def validate_bus_name
+    errors.add(:name, 'must be a string') unless name_before_type_cast.is_a?(String)
+
+    return unless name_before_type_cast.is_a?(String)
+
+    if name.size > 40 
+      errors.add(:name, 'should not be greater than 50 characters')
+    elsif name.size <= 2
+      errors.add(:name, 'should be valid and greater than 2 characters')
+    end
+  end
 end
