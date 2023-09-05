622bb5a830c86473e9b8d237b44abfad6fdf7c14
Aakash Verma <dev.aakashv@gmail.com>
2023-09-06 01:31:55 +0530

    Add the custom validation in the `reservation` table

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,6 +1,37 @@
 class Reservation < ApplicationRecord
-  validates :berth_number, presence: true, numericality: true
+  validates :berth_number, presence: true
   validates :refund_status, presence: true
-  validates :amount_paid, presence: true, numericality: { greater_than_or_equal_to: 0 }
-  validates :status, inclusion: { in: [true, false], message: 'must be a boolean' }
+  validates :amount_paid, presence: true
+  validates :status, presence: true
+
+  validate :validate_berth_number
+  validate :validate_refund_status
+  validate :validate_amount_paid
+  validate :validate_status
+
+  private
+
+  def validate_berth_number
+    errors.add(:berth_number, 'must be an integer') unless berth_number_before_type_cast.is_a?(Integer)
+  end
+
+  def validate_amount_paid
+    errors.add(:amount_paid, 'must be an integer') unless amount_paid_before_type_cast.is_a?(Integer)
+  end
+
+  def validate_refund_status
+    errors.add(:refund_status, 'must be a string') unless refund_status_before_type_cast.is_a?(String)
+
+    return unless refund_status_before_type_cast.is_a?(String)
+
+    if refund_status.size > 250
+      errors.add(:refund_status, 'should not be greater than 250 characters')
+    elsif refund_status.size <= 30
+      errors.add(:refund_status, 'should be greater than 30 characters')
+    end
+  end
+
+  def validate_status
+    errors.add(:refund_status, 'must be either true or false') unless status_before_type_cast.is_a?(TrueClass) || status_before_type_cast.is_a?(FalseClass)
+  end
 end
