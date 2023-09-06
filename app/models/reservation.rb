5803f92e7da68d36bfe024d2d87edfbc597158d8
Aakash Verma <dev.aakashv@gmail.com>
2023-09-06 15:36:43 +0530

    Improve the functionalities

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,41 +1,45 @@
 class Reservation < ApplicationRecord
   validates :berth_number, presence: true
-  validates :refund_status, presence: true
   validates :amount_paid, presence: true
 
-  validate :validate_berth_number
-  validate :validate_refund_status
-  validate :validate_amount_paid
-  validate :validate_status
+  validate :validate_reservation_berth_number
+  validate :validate_reservation_refund_status
+  validate :validate_reservation_amount_paid
+  validate :validate_reservation_status
 
   belongs_to :user
   belongs_to :bus
 
   private
 
-  def validate_berth_number
+  def validate_reservation_berth_number
     errors.add(:berth_number, 'must be an integer') unless berth_number_before_type_cast.is_a?(Integer)
   end
 
-  def validate_amount_paid
-    errors.add(:amount_paid, 'must be an integer') unless amount_paid_before_type_cast.is_a?(Integer) || amount_paid_before_type_cast.is_a?(Float)
+  def validate_reservation_amount_paid
+    rreturn if amount_paid.blank? # Allow nil or empty value
+
+    p amount_paid.class
+    unless amount_paid.is_a?(Integer) || amount_paid.is_a?(BigDecimal) || amount_paid.is_a?(Decimal) || amount_paid.is_a?(Float)
+      errors.add(:amount_paid, 'must be an integer or a float')
+    end
   end
 
-  def validate_refund_status
+  def validate_reservation_refund_status
     errors.add(:refund_status, 'must be a string') unless refund_status_before_type_cast.is_a?(String)
 
     return unless refund_status_before_type_cast.is_a?(String)
 
     if refund_status.size > 250
       errors.add(:refund_status, 'should not be greater than 250 characters')
-    elsif refund_status.size <= 30
-      errors.add(:refund_status, 'should be greater than 30 characters')
+    elsif refund_status.size <= 10
+      errors.add(:refund_status, 'should be greater than 10 characters')
     end
   end
 
-  def validate_status
-    unless status_before_type_cast.kind_of?(TrueClass) ||
-           status_before_type_cast.kind_of?(FalseClass)
+  def validate_reservation_status
+    unless status.kind_of?(TrueClass) ||
+          status.kind_of?(FalseClass)
       errors.add(:status, 'must be a boolean either true or false')
     end
   end
