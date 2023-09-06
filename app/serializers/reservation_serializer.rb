9c9e1b49258f57183fb13cd346937c49d1858695
Aakash Verma <dev.aakashv@gmail.com>
2023-09-06 15:37:03 +0530

    Add more attributes for more information

--- app/serializers/reservation_serializer.rb
+++ app/serializers/reservation_serializer.rb
@@ -1,4 +1,4 @@
 class ReservationSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :berth_number, :refund_status, :amount_paid, :status
+  attributes :berth_number, :refund_status, :amount_paid, :status, :user, :bus
 end
